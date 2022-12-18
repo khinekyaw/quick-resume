67a8ff354e8560612dffefe17961025f81f34993
danielrio <danielrio126@gmail.com>
2022-12-18 19:30:37 +0630

    Move ThreeBlock to icons

---
+++ components/icons/ThreeBlock.js
@@ -0,0 +1,15 @@
+const ThreeBlock = () => (
+  <svg
+    width='25'
+    height='18'
+    viewBox='0 0 25 18'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+  >
+    <rect x='0.5' y='0.5' width='6' height='17' rx='1.5' stroke='black' />
+    <rect x='9.5' y='0.5' width='6' height='17' rx='1.5' stroke='black' />
+    <rect x='18.5' y='0.5' width='6' height='17' rx='1.5' stroke='black' />
+  </svg>
+)
+
+export default ThreeBlock
